b6e5545000a41bae74daae90c14cb265f1dcf57e
Larissa Cancella <larissabotelho2021@gmail.com>
2021-10-16 20:50:18 -0300

    Ajustes na classe RepositorioDados, inclusao de metodos para salvar e atualizar bilhete, e atualizar passageiro

--- src/faeterj/prj/vavatur/banco/RepositorioDados.java
+++ src/faeterj/prj/vavatur/banco/RepositorioDados.java
@@ -8,13 +8,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
 
 public class RepositorioDados {
 
     private RepositorioDados() {}
 
-    private static RepositorioDados instancia = new RepositorioDados();
+    private static final RepositorioDados instancia = new RepositorioDados();
 
     public static RepositorioDados getInstancia() {
         return instancia;
@@ -22,31 +21,77 @@ public class RepositorioDados {
 
     public Bilhete buscarBilhetePorCodigo(String codigo) throws SQLException {
         Connection c = ConexaoComOBanco.getConnection();
-        PreparedStatement cmd = c.prepareStatement("select codigo, assento, * from bilhete where id = ?");
+        PreparedStatement cmd = c.prepareStatement("select * from bilhete where codigo = ?");
         cmd.setString(1, codigo);
         cmd.execute();
 
+        Bilhete bilhete = new Bilhete();
+        Passageiro passageiro = new Passageiro();
+        Linha linha = new Linha();
+
         try (ResultSet rs = cmd.getResultSet()) {
             if (rs.next()) {
-                Bilhete b = new Bilhete();
-                b.setCodigo(rs.getString(1));
-                // ...
-                return b;
+                bilhete.setCodigo(rs.getString(1));
+                bilhete.setAssento(rs.getString(2));
+                PreparedStatement passageiros = c.prepareStatement("select * from passageiro where id = ?");
+                cmd.setString(1, rs.getString(3));
+                cmd.execute();
+                try (ResultSet rsPassageiros = passageiros.getResultSet()) {
+                    if (rsPassageiros.next()) {
+                        passageiro.setId(rsPassageiros.getInt(1));
+                        passageiro.setNome(rsPassageiros.getString(2));
+                        passageiro.setCpf(rsPassageiros.getString(3));
+                        passageiro.setTelefone(rsPassageiros.getString(4));
+                        passageiro.setEmail(rsPassageiros.getString(3));
+                    }
+                }
+                PreparedStatement linhas = c.prepareStatement("select * from linha where id = ?");
+                cmd.setString(1, rs.getString(4));
+                cmd.execute();
+                try (ResultSet rsLinhas = linhas.getResultSet()) {
+                    if (rsLinhas.next()) {
+                        linha.setId(rsLinhas.getInt(1));
+                        linha.setOrigem(rsLinhas.getString(2));
+                        linha.setDestino(rsLinhas.getString(2));
+                        //linha.setHoraEmbarque(rsLinhas.getString(3));
+                        //linha.setHoraPartida(rsLinhas.getString(4));
+                    }
+                }
+                bilhete.setPassageiro(passageiro);
+                bilhete.setLinha(linha);
+                //bilhete.setHoraMarcadaAssento(null);
+                return bilhete;
             }
         }
         throw new RuntimeException("Bilhete com código " + codigo + " não encontrado!");
     }
 
-    public void salvarBilhete(Bilhete bilhete) {
-
+    public void salvarBilhete(Bilhete bilhete) throws SQLException {
+        Connection c = ConexaoComOBanco.getConnection();
+        PreparedStatement cmd = c.prepareStatement("insert into bilhete values(?,?,?,?,?)");
+        cmd.setString(1, bilhete.getCodigo());
+        cmd.setString(2, bilhete.getAssento());
+        cmd.setInt(3, bilhete.getPassageiro().getId());
+        cmd.setInt(4, bilhete.getLinha().getId());
+        //cmd.setDate(5, bilhete.getHoraMarcadaAssento());
+        cmd.execute();
     }
 
-    public List<Passageiro> listarPassageirosLinha(Linha linha) {
-        return null;
+    public void atualizarPassageiro(Passageiro passageiro) throws SQLException {
+        Connection c = ConexaoComOBanco.getConnection();
+        PreparedStatement cmd = c.prepareStatement("update passageiro set nome = ?, cpf = ? where id = ?");
+        cmd.setString(1, passageiro.getNome());
+        cmd.setString(2, passageiro.getCpf());
+        cmd.setInt(3, passageiro.getId());
+        cmd.execute();
     }
 
-    public static RepositorioDados getInstance() {
-        return instancia;
+    public void salvarAssento(Bilhete bilhete) throws SQLException {
+        Connection c = ConexaoComOBanco.getConnection();
+        PreparedStatement cmd = c.prepareStatement("update bilhete set assento = ? where codigo = ?");
+        cmd.setString(1, bilhete.getAssento());
+        cmd.setString(2, bilhete.getCodigo());
+        cmd.execute();
     }
 
 }
